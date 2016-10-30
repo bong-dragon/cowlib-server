06a3a1595a0f7644d39ff30064384ca45902f21d
jb.squee <jb.squee@daum.net>
2016-10-30 22:03:01 +0900

    client 리팩토링

--- src/main/java/com/cowlib/client/DaumBookClient.java
+++ src/main/java/com/cowlib/client/DaumBookClient.java
@@ -3,24 +3,57 @@ package com.cowlib.client;
 import com.cowlib.model.Book;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * Created by KAKAO on 2016. 10. 30..
- */
-public class DaumBookApiClient {
+@Component
+public class DaumBookClient {
+    private static final String APIS_DAUM_NET = "apis.daum.net";
+    private static final String HTTPS = "https";
+    private static final String SEARCH_BOOK = "/search/book";
+
+    @Value("${daum.api.book}")
+    private String apikey;
+
+    private RestTemplate restTemplate = new RestTemplate();
+
+    public DaumBookClient() {
+    }
+
+    public DaumBookClient(String apikey) {
+        this.apikey = apikey;
+    }
+
     public List<Book> search(String q) {
-        RestTemplate restTemplate = new RestTemplate();
-        String uri = "https://apis.daum.net/search/book?output=json&apikey=0e07fcf5690bd28508f96c9415ecdeeb&q=" + q;
-        String result = restTemplate.getForObject(uri, String.class);
+        String uri = buildUri(q);
+        String jsonString = restTemplate.getForObject(uri, String.class);
+
+        return parse(jsonString);
+    }
+
+    private String buildUri(String q) {
+        UriComponentsBuilder builder = UriComponentsBuilder.newInstance();
+        return builder.scheme(HTTPS)
+                .host(APIS_DAUM_NET)
+                .path(SEARCH_BOOK)
+                .queryParam("apikey", apikey)
+                .queryParam("output", "json")
+                .queryParam("q", q)
+                .toUriString();
+    }
+
+    private List<Book> parse(String result) {
         Map<String, Object> map = new HashMap<>();
         ObjectMapper mapper = new ObjectMapper();
-
         try {
             //convert JSON string to Map
             map = mapper.readValue(result, new TypeReference<HashMap<String, Object>>() {
@@ -30,12 +63,10 @@ public class DaumBookApiClient {
         }
 
         Map<String, Object> channel = (Map<String, Object>) map.get("channel");
-
         List<Map<String, Object>> List = (java.util.List<Map<String, Object>>) channel.get("item");
 
         List<Book> books = new ArrayList<>();
         for (Map<String, Object> jsonBook : List) {
-            System.out.println(jsonBook);
             String isbn = (String) jsonBook.get("isbn");
             String isbn13 = (String) jsonBook.get("isbn13");
             String title = (String) jsonBook.get("title");
---
+++ src/test/java/com/cowlib/client/DaumBookClientTest.java
@@ -0,0 +1,31 @@
+package com.cowlib.client;
+
+import com.cowlib.model.Book;
+
+import com.cowlib.util.PropertyFileLoader;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Properties;
+
+import static org.assertj.core.api.Assertions.assertThat;
+public class DaumBookClientTest {
+
+    private String apikey;
+
+    @Before
+    public void setUp(){
+        Properties properties = PropertyFileLoader.getProperties("application-local.properties");
+        apikey = properties.getProperty("daum.api.book");
+    }
+
+    @Test
+    public void search() throws Exception {
+        DaumBookClient client = new DaumBookClient(apikey);
+        List<Book> books = client.search("Hello");
+
+        assertThat(books.size()).isEqualTo(10);
+    }
+
+}
