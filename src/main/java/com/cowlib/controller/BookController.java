c385c4f31b868a35637ff1ede08496542f6d8954
jb.squee <jb.squee@daum.net>
2016-10-30 23:30:39 +0900

    #3 book repository / properties 삭제

--- src/main/java/com/cowlib/controller/BookController.java
+++ src/main/java/com/cowlib/controller/BookController.java
@@ -2,6 +2,7 @@ package com.cowlib.controller;
 
 import com.cowlib.client.DaumBookClient;
 import com.cowlib.model.Book;
+import com.cowlib.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,10 +17,21 @@ public class BookController {
     @Autowired
     DaumBookClient client;
 
+    @Autowired
+    BookRepository bookRepository;
+
+
     @RequestMapping("books/search")
     public List<Book> search(@RequestParam(value = "q") String q) {
         List<Book> books = client.search(q);
 
+        for (Book book : books) {
+            Book alreaySaved = bookRepository.select(book);
+
+            if(alreaySaved == null){
+                bookRepository.insert(book);
+            }
+        }
         return books;
     }
 }
--- src/main/java/com/cowlib/model/Book.java
+++ src/main/java/com/cowlib/model/Book.java
@@ -64,8 +64,13 @@ public class Book {
         return this;
     }
 
-    public String getCover_url() {
+    public String getCoverUrl() {
         return coverUrl;
+    }
+
+    public Book setCoverUrl(String coverUrl) {
+        this.coverUrl = coverUrl;
+        return this;
     }
 
     @Override
@@ -77,14 +82,36 @@ public class Book {
                 ", author='" + author + '\'' +
                 ", description='" + description + '\'' +
                 ", publisher='" + publisher + '\'' +
-                ", cover_url='" + coverUrl + '\'' +
+                ", coverUrl='" + coverUrl + '\'' +
                 '}';
     }
 
-    public Book setCoverUrl(String coverUrl) {
-        this.coverUrl = coverUrl;
-        return this;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        if (isbn != null ? !isbn.equals(book.isbn) : book.isbn != null) return false;
+        if (isbn13 != null ? !isbn13.equals(book.isbn13) : book.isbn13 != null) return false;
+        if (title != null ? !title.equals(book.title) : book.title != null) return false;
+        if (author != null ? !author.equals(book.author) : book.author != null) return false;
+        if (description != null ? !description.equals(book.description) : book.description != null) return false;
+        if (publisher != null ? !publisher.equals(book.publisher) : book.publisher != null) return false;
+        return coverUrl != null ? coverUrl.equals(book.coverUrl) : book.coverUrl == null;
+
     }
 
-
+    @Override
+    public int hashCode() {
+        int result = isbn != null ? isbn.hashCode() : 0;
+        result = 31 * result + (isbn13 != null ? isbn13.hashCode() : 0);
+        result = 31 * result + (title != null ? title.hashCode() : 0);
+        result = 31 * result + (author != null ? author.hashCode() : 0);
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        result = 31 * result + (publisher != null ? publisher.hashCode() : 0);
+        result = 31 * result + (coverUrl != null ? coverUrl.hashCode() : 0);
+        return result;
+    }
 }
