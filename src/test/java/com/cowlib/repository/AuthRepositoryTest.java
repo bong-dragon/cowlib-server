6ffdfd91be5002af5e2618d23db1507798c1fc0b
jbsquee <jb.squee@kakaocorp.com>
2016-10-30 15:43:34 +0900

    #2 properties 분리

--- src/test/java/com/cowlib/repository/AuthRepositoryTest.java
+++ src/test/java/com/cowlib/repository/AuthRepositoryTest.java
@@ -4,12 +4,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import com.Application;
 import com.cowlib.User;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
+@TestPropertySource("/application-local.properties")
 public class AuthRepositoryTest {
 
 	@Autowired
