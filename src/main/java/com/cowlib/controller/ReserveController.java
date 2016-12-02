66a8cda9e8b34666aa73c502227f40e28ea413b1
erin.lee <erin.lee@navercorp.com>
2016-12-02 21:12:14 +0900

    [#39] reserve api 버그 수정 및 중복방지

--- src/main/java/com/cowlib/controller/ReserveController.java
+++ src/main/java/com/cowlib/controller/ReserveController.java
@@ -1,14 +1,20 @@
 package com.cowlib.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import com.cowlib.code.ReserveStatus;
 import com.cowlib.model.Reserve;
 import com.cowlib.repository.ReserveRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/callNumbers/{callNumberId}/reserve")
 public class ReserveController {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     private ReserveRepository reserveRepository;
@@ -16,6 +22,12 @@ public class ReserveController {
     @PostMapping
     public Reserve reserve(Reserve reserve) {
         reserve.setStatus(ReserveStatus.예약함.getCode());
+        Reserve alreadyReserve = reserveRepository.selectByCallNumberIdAndReserverIdAndStatus(reserve);
+        if (alreadyReserve != null){
+           logger.debug("{}님, {}을 이미 예약 하셨어요", alreadyReserve.getReserverId(), alreadyReserve.getCallNumberId());
+            return reserve;
+        }
+
         reserveRepository.insert(reserve);
         return reserve;
     }
--- src/main/java/com/cowlib/repository/ReserveRepository.java
+++ src/main/java/com/cowlib/repository/ReserveRepository.java
@@ -1,8 +1,14 @@
 package com.cowlib.repository;
 
-import com.cowlib.model.Reserve;
-import org.apache.ibatis.annotations.*;
 import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+import com.cowlib.model.Reserve;
 
 @Mapper
 public interface ReserveRepository {
@@ -14,15 +20,18 @@ public interface ReserveRepository {
     @Select("select * from reserve_history where id=#{id}")
     Reserve select(Reserve reserve);
 
+    @Select("select * from reserve_history where call_number_id=#{callNumberId} and reserver_id=#{reserverId} and status=#{status}")
+    Reserve selectByCallNumberIdAndReserverIdAndStatus(Reserve reserve);
+
     @Update("update reserve_history set status=#{status} where id=#{id}")
     void update(Reserve reserve);
 
-    @Update("update reserve_history set status=#{status} where id=#{callNumberId}")
+    @Update("update reserve_history set status=#{status} where call_number_id=#{callNumberId}")
     void updateByCallNumberId(Reserve reserve);
 
     @Delete("delete from reserve_history where id=#{id}")
     void delete(Reserve reserve);
 
-    @Select("select * from reserve_history where id=#{callNumberId} and status=#{status} order by id desc")
+    @Select("select * from reserve_history where call_number_id=#{callNumberId} and status=#{status} order by call_number_id desc")
     List<Reserve> selectByCallNumberIdAndStatus(Reserve reserve);
 }
