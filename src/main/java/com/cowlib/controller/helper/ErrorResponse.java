b4c96822d557783167ee309b04d5a8f79c48c50c
jbsquee <jb.squee@daum.net>
2016-12-06 20:13:55 +0900

    #2 복잡한 패턴 버리고 json 응답

--- src/main/java/com/cowlib/controller/helper/ErrorResponse.java
+++ src/main/java/com/cowlib/controller/helper/ErrorResponse.java
@@ -1,27 +1,14 @@
 package com.cowlib.controller.helper;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import lombok.Data;
 
-import javax.servlet.http.HttpServletRequest;
+@Data
+public class ErrorResponse {
+    private int errorCode;
+    private String message;
 
-public class ErrorResponse extends ResponseEntity {
-    private HttpServletRequest request;
-    private Throwable exception;
-
-    public ErrorResponse(HttpServletRequest request, HttpStatus httpStatus, Exception exception) {
-        super(exception.getMessage(), httpStatus);
-        this.request = request;
-        this.exception = exception;
-
-    }
-
-    public ErrorResponse logError(LoggingProvider provider) {
-        provider.getLogger().error(this.getLoggingMessage(), this.exception);
-        return this;
-    }
-
-    public String getLoggingMessage() {
-        return this.exception.getCause() != null ? this.exception.getClass().getSimpleName() + " caused by " + this.exception.getCause().getClass().getSimpleName() + ": " + this.exception.getMessage() : this.exception.getClass().getSimpleName() + ": " + this.exception.getMessage();
+    public ErrorResponse(int errorCode, String message) {
+        this.errorCode = errorCode;
+        this.message = message;
     }
 }
--- src/main/java/com/cowlib/controller/helper/LoggingProvider.java
+++
@@ -1,7 +0,0 @@
-package com.cowlib.controller.helper;
-import org.slf4j.Logger;
-
-public interface LoggingProvider {
-
-    Logger getLogger();
-}
