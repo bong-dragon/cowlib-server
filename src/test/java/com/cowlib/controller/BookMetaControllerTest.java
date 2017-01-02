d19c58f53d2e14d8360dffec2f2cd8ecffe117d0
jb.squee <jb.squee@daum.net>
2017-01-02 21:17:22 +0900

    #23 profile 분리

--- src/test/java/com/cowlib/controller/BookMetaControllerTest.java
+++ src/test/java/com/cowlib/controller/BookMetaControllerTest.java
@@ -10,7 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -23,7 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
-@TestPropertySource("/application-local.properties")
+@ActiveProfiles("local")
 public class BookMetaControllerTest {
 private static final Logger LOGGER = LoggerFactory.getLogger(BookMetaControllerTest.class);
 
